b79a7401a67c737e9388a9c9bbd1bad881dc754d
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-21 00:06:35 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -2,11 +2,9 @@ count = 10
 exit_number = 0
 
 while count < 11 do
-  
-puts count -= 1   
-break if  count == exit_number
+  break if  count == exit_number
 puts "Happy New Year!"
-
+count -= 1 
 
 end
 
